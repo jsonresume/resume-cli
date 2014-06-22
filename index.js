1e02e1e8d970398462ac3f3bfb131807817aefd9
Roland Sharp <rolandnsharp@gmail.com>
2014-06-22 13:43:50 +1000

    resume export working for html and txt

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
 
-var resumeToText = require('resume-to-text');
-var resumeToHtml = require('resume-to-html');
 var program = require('commander');
 var fs = require('fs');
 var init = require('./lib/init');
 var test = require('./lib/test');
 var publish = require('./lib/publish');
+var exportJson = require('./lib/exportJson');
 var colors = require('colors');
-
 
 var resumeData = JSON.parse(fs.readFileSync('resume.json', 'utf8'));
 
 program
     .version('0.0.1')
-    .option('-f, --format [format]', 'Add the specified format of file [format]', 'html')
-    .option('--force [force]', 'Force publish [force]', false);
+    .option('-f, --force [force]', 'Force publish [force]', false);
 
 program
     .command('init')
@@ -40,47 +37,19 @@ program
 
 program
     .command('export <fileName>')
-    .description('Publish resume.json')
+    .description('Export int .html, .txt or .pdf')
     .action(function(fileName) {
-
-        console.log(fileName);
+        exportJson(resumeData, fileName);
     });
 
-
 program.parse(process.argv);
-var argumentZero = program.args[0];
 
+// var argumentZero = program.args[0];
 
+// if resume is run with no commands
 if (!program.args.length) {
     console.log(' \n ');
     console.log('Please type:', 'resume --help'.cyan, 'for information on using resume-cli');
     console.log('or:', 'resume init'.cyan, 'to initialize a new resume.json and start right away.');
     process.exit();
 }
-
-
-var resumeOutput = 'resume';
-
-
-if (program.format === 'html') {
-
-    if (!resumeOutput) {
-        // resumeOutput = argumentZero.replace("json", "html");
-        resumeOutput = 'resume.html';
-    }
-    resumeToHtml(resumeData, function(TextResume) {
-        fs.writeFileSync(resumeOutput, TextResume, 'utf8');
-    });
-
-}
-
-// else if (program.format === 'txt') {
-
-//     if (!resumeOutput) {
-//         resumeOutput = argumentZero.replace("json", "txt");;
-//     }
-//     resumeToText(resumeData, function(TextResume) {
-//         fs.writeFileSync(resumeOutput, TextResume, 'utf8');
-//     });
-
-// }
