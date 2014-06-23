223567c96fb2cc9084f13d80ecfe699806aacd82
Roland Sharp <rolandnsharp@gmail.com>
2014-06-23 20:16:51 +1000

    match index

--- index.js
+++ index.js
@@ -6,19 +6,18 @@ var init = require('./lib/init');
 var test = require('./lib/test');
 var publish = require('./lib/publish');
 var register = require('./lib/register');
+var exportResume = require('./lib/exportResume');
 var colors = require('colors');
-var resumeToText = require('resume-to-text');
-var resumeToHtml = require('resume-to-html');
-var html5pdf = require("html5-to-pdf");
+
 
 if (fs.existsSync('./resume.json')) {
     var resumeData = JSON.parse(fs.readFileSync('resume.json', 'utf8'));
 } else {
-    resumeData = {};
+    resumeData = false;
 }
 
 program
-    .version('0.0.4')
+    .version('0.0.9')
     .option('-f, --force [force]', 'Force publish [force]', false)
 
 program
@@ -32,22 +31,36 @@ program
     .command('test')
     .description('Test resume.json')
     .action(function() {
-        test.validate(resumeData);
+        if (resumeData === false) {
+            console.log('There is no resume.json file located in this directory'.yellow);
+            console.log('Type:'.cyan, 'resume init', 'to initialize a new resume'.cyan);
+        } else {
+            test.validate(resumeData);
+        }
     });
 
 program
     .command('publish')
     .description('Publish resume.json')
     .action(function() {
-        publish(resumeData, program.force);
+        if (resumeData === false) {
+            console.log('There is no resume.json file located in this directory'.yellow);
+            console.log('Type:'.cyan, 'resume init', 'to initialize a new resume'.cyan);
+        } else {
+            publish(resumeData, program.force);
+        }
     });
 
 program
-    .command('export <fileName>')
+    .command('export [fileName]')
     .description('Export int .html, .txt or .pdf')
     .action(function(fileName) {
-        console.log('fjdkls')
-        exports(resumeData, fileName);
+        if (resumeData === false) {
+            console.log('There is no resume.json file located in this directory'.yellow);
+            console.log('Type:'.cyan, 'resume init', 'to initialize a new resume'.cyan);
+        } else {
+            exportResume(resumeData, fileName);
+        }
     });
 
 program
@@ -59,57 +72,17 @@ program
 
 program.parse(process.argv);
 
-// if resume is run with no commands
+//if run with no commands
 if (!program.args.length) {
-
     console.log('resume-cli'.cyan, '\n');
     console.log('Please type:', 'resume --help'.cyan, 'for information on using resume-cli');
     console.log('or:', 'resume init'.cyan, 'to initialize a new resume.json and start right away.');
     process.exit();
 }
 
-function exports(resumeData, fileName) {
-
-    splitFileName = fileName.split('.');
-    var fileExtension = splitFileName[1];
-
-    if (!fileName) {
-        console.log("Please provide a file name for your resume");
-        console.log("");
-        process.exit();
-
-        // } else if (splitFileName.length === ) {
-        //     console.log('Please enter a valid file extexions (.html .txt .pdf)');
-    }
-
-    switch (fileExtension) {
-
-        case 'html':
-            resumeToHtml(resumeData, function(htmlResume) {
-
-                fs.writeFileSync(fileName, htmlResume, 'utf8');
-
-                console.log('Done! Please find your generated .html resume at:'.green, process.cwd() + '/resume.html');
-                process.exit();
-            });
-            break;
-        case 'txt':
-            resumeToText(resumeData, function(txtResume) {
-                fs.writeFileSync(fileName, txtResume, 'utf8');
-                console.log('Done! Please find your generated .html resume at:'.green, process.cwd() + '/resume.json');
-                process.exit();
-            });
-            break;
-        case 'pdf':
-            resumeToHtml(resumeData, function(htmlResume) {
-                fs.writeFileSync(fileName, htmlResume, 'utf8');
-
-                html5pdf().from(splitFileName[0] + '.html').to(splitFileName[0] + '.pdf', function() {
-                    console.log('Done! Please find your generated .html resume at:'.green, process.cwd() + '/resume.json');
-                    process.exit();
-
-                })
-            });
-            break;
-    }
-}
+//todo
+//resume to pdf and markdown
+//email already in use validation
+// reginster success handeling
+// markdown to html
+// connect travis coffee script
