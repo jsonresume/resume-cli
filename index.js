89f5c37fe92a8ef048ccbcbaf5afcd5d268f2e3b
Santosh Shaastry <csshaastry@gmail.com>
2015-01-29 23:40:32 +0530

    Changes to display error when export HTML format results in some error,

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ var pkg = require('./package.json');
 var lib = require('./lib');
 var program = require('commander');
 var async = require('async');
-var colors = require('colors');
 var chalk = require('chalk');
-var read = require('read');
 
 program
     .version(pkg.version)
-    .option('-t, --theme <theme name>', 'Specify theme for export or publish (modern, traditional, crisp)', 'flat')
+    .option('-t, --theme <theme name>', 'Specify theme for serve, export or publish (modern, traditional, crisp)', 'flat')
     .option('-F, --force', 'Used by `publish` - bypasses schema testing.')
     .option('-f, --format <file type extension>', 'Used by `export`.')
     .option('-p, --port <port>', 'Used by `serve` (default: 4000)', 4000)
@@ -61,7 +59,11 @@ async.waterfall(lib.waterfallArray, function(err, results) {
         .description('Export locally to .html, .md or .pdf. Supply a --format <file format> flag and argument to specify export format.')
         .action(function(fileName) {
             lib.exportResume(results.resumeJson, fileName, program, function(err, fileName, format) {
-                console.log(chalk.green('\nDone! Find your new', format, 'resume at', process.cwd() + '\\' + fileName + format));
+			    if(err) {
+				  console.log(chalk.red(err));
+				} else {
+				  console.log(chalk.green('Done! Find your new', format, 'resume at', process.cwd() + '\\' + fileName + format));
+				}
             });
         });
 
