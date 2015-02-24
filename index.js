66f6ea5c4b6777b5405d1f5f1fc33244ac9a8db7
Dan Levy <justsml@gmail.com>
2015-02-24 03:09:53 -0700

    Removed the export --format option and related menu interface

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ program
     .version(pkg.version)
     .option('-t, --theme <theme name>', 'Specify theme for export or publish (modern, traditional, crisp)', 'flat')
     .option('-F, --force', 'Used by `publish` - bypasses schema testing.')
-    .option('-f, --format <file type extension>', 'Used by `export`.')
     .option('-p, --port <port>', 'Used by `serve` (default: 4000)', 4000)
     .option('-s, --silent', 'Used by `serve` to tell it if open browser auto or not.', false);
 
@@ -58,10 +57,10 @@ async.waterfall(lib.waterfallArray, function(err, results) {
 
     program
         .command('export [fileName]')
-        .description('Export locally to .html, .md or .pdf. Supply a --format <file format> flag and argument to specify export format.')
+        .description('Export locally to .html or .pdf file. (File name should include an extension.)')
         .action(function(fileName) {
             lib.exportResume(results.resumeJson, fileName, program, function(err, fileName, format) {
-                console.log(chalk.green('\nDone! Find your new', format, 'resume at', process.cwd() + '/' + fileName + format));
+                console.log(chalk.green('\nDone! Find your new', format, 'resume at', process.cwd() + '/' + fileName));
             });
         });
 
