636f9b1b5872dc06d600af80466653491a3b1ab7
Roland Sharp <rolandnsharp@gmail.com>
2015-05-10 15:21:13 +1000

    Refactor the register function.
    - modularise
    - add test file
    - made validations module

--- test/index.js
+++
@@ -1,8 +0,0 @@
-var lib = require('../lib');
-var test = require('tape');
-var fs = require('fs');
-
-test('test.', function(t) {
-    t.plan(1);
-    t.ok(true, 'todo: write tests....');
-});
---
+++ test/register-test.js
@@ -0,0 +1,14 @@
+var should = require('should');
+
+describe("Register tests", function() {
+//
+	it('should register new user', function(done) {
+//
+//
+//
+//
+		done();
+	});
+//
+});
+console.log('jeje');
