d7528ad310942e0fb76b54571534af4e00e3ff0e
pinage404 <pinage404@gmail.com>
2018-08-12 16:50:16 +0200

    feat(theme-as-promise): allow theme to render with promise

---
+++ test/renderHtml.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const should = require('should');
+const renderHtml = require('../lib/render-html');
+
+describe('renderHtml', () => {
+  const resumeJson = {
+    basics: {
+      name: 'test',
+      label: 'Programmer',
+      email: 'test4@test.com',
+    },
+  };
+
+  it('should reject when theme is not availlable', async () => {
+    const theme = 'unkown';
+
+    const result = renderHtml(resumeJson, theme);
+
+    return result.should.be.rejected();
+  });
+
+  describe('should render html when theme is availlable', () => {
+    it('with long theme name', async () => {
+      const theme = 'jsonresume-theme-flat';
+
+      const html = await renderHtml(resumeJson, theme);
+
+      return html.should.startWith('<!doctype html>');
+    });
+
+    it('with short theme name', async () => {
+      const theme = 'flat';
+
+      const html = await renderHtml(resumeJson, theme);
+
+      return html.should.startWith('<!doctype html>');
+    });
+  });
+});
