00787a406a1e396a266f92127bdfb8cfe387cbbe
pinage404 <pinage404@gmail.com>
2018-08-15 16:01:16 +0200

    test(cli): add tests for option to define resume path in CLI

---
+++ test/get-resume-path.js
@@ -0,0 +1,40 @@
+const should = require('should')
+const getResumePath = require('../lib/pre-flow/get-resume-path')
+
+describe('getResumePath', () => {
+  it('should give "./resume.json" by default', () => {
+    const resumePath = getResumePath([])
+
+    resumePath.should.equal('./resume.json')
+  })
+
+  describe('should give resume path given by arguments', () => {
+    it('with "-r=<PATH>" option', () => {
+      const argv = [
+        '/usr/bin/node',
+        '/home/user/my-resume/node_modules/.bin/resume',
+        'export',
+        'resume.html',
+        '-r=my-resume.json',
+      ]
+
+      const resumePath = getResumePath(argv)
+
+      resumePath.should.equal('my-resume.json')
+    })
+
+    it('with "--resume=<PATH>" option', () => {
+      const argv = [
+        '/usr/bin/node',
+        '/home/user/my-resume/node_modules/.bin/resume',
+        'export',
+        'resume.html',
+        '--resume=my-resume.json',
+      ]
+
+      const resumePath = getResumePath(argv)
+
+      resumePath.should.equal('my-resume.json')
+    })
+  })
+})
