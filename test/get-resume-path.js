60649ca161dd9fd99aebe9cb2cd63375f1a53663
pinage404 <pinage404@gmail.com>
2018-08-15 16:36:41 +0200

    feat(cli): should accept "resume" CLI's option even with space

--- test/get-resume-path.js
+++ test/get-resume-path.js
@@ -36,6 +36,36 @@ describe('getResumePath', () => {
 
       resumePath.should.equal('my-resume.json')
     })
+
+    it('with "-r <PATH>" option', () => {
+      const argv = [
+        '/usr/bin/node',
+        '/home/user/my-resume/node_modules/.bin/resume',
+        'export',
+        'resume.html',
+        '-r',
+        'my-resume.json',
+      ]
+
+      const resumePath = getResumePath(argv)
+
+      resumePath.should.equal('my-resume.json')
+    })
+
+    it('with "--resume <PATH>" option', () => {
+      const argv = [
+        '/usr/bin/node',
+        '/home/user/my-resume/node_modules/.bin/resume',
+        'export',
+        'resume.html',
+        '--resume',
+        'my-resume.json',
+      ]
+
+      const resumePath = getResumePath(argv)
+
+      resumePath.should.equal('my-resume.json')
+    })
   })
 
   it('should give default value even when the folder contains "-r"', () => {
