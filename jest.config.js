245ea9495b9070ca616a9089b5efe56cf09067fd
Thomas Hallock <antialias@users.noreply.github.com>
2020-12-10 17:16:11 -0600

    get resume supports input stream, yaml, and quaff (#445)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  maxWorkers: 1, // maintains isTTY, since using a single worker will not cause jest to use child processes to run the tests
   setupFilesAfterEnv: ['jest-extended'],
   roots: ['<rootDir>/lib'],
 };
